75e53fbb7fd4d4edf3cac21d03eff04b65b1a147
breezedu <breeze.du@gmail.com>
2014-09-03 11:57:02 -0400

    updata GetXandY.java class

--- GetXandY.java
+++ GetXandY.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 /*****
  * after Relative Distance Orc, all relative distance are available;
  * transfer all the relative distance to (x, y) pairs;
+ * save the coordinates pairs into orc_write_output_small.txt;
  * 
  * create an array a[2001]: 
  * a[0] -> relative_distance at -1000;
@@ -24,31 +25,32 @@ public class GetXandY {
 		System.out.println("Read data from orc_output.csv.");
 		
 		//1st, read relative distance data from orc_output.csv file;
-		Scanner ORC_reader = new Scanner(new File("orc_output.csv"));
+		Scanner ORC_output_reader = new Scanner(new File("orc_output_small.csv"));
 		
 		//2nd, create an array of relate_distance a[2001];
 		int[] a = new int[2001];
 		
-		String title = ORC_reader.nextLine();
+		String title = ORC_output_reader.nextLine();
 		System.out.println(" " + title +": ");
 		
-		while(ORC_reader.hasNextLine()){
+		while(ORC_output_reader.hasNextInt()){
 			
-			String line = ORC_reader.nextLine();
-			String[] split = line.split(",");
-			int len = split.length;
-			
-			int relative_distance = Integer.parseInt(split[len-1]);
-			
-			int index = relative_distance + 1000;
-			
-			if(index>=0 && index<2001)
-				a[index]++;
+				int relative_distance = ORC_output_reader.nextInt();
+				
+				int index = relative_distance + 1000;
+				
+				//only save [-1000, +1000]
+				//at first, I tried to save all relative distance to the data_file, 
+				//then the file size was over 600M
+				//so later I just put numbers between -1000 and +1000 into the data_file;
+				if(index>=0 && index<2001)
+					a[index]++;
+		
 			
 		}//end while ORC_reader.hasNextLine() loop;
 		
 		
-		ORC_reader.close();
+		ORC_output_reader.close();
 		
 		
 		//3rd, printout the array:
@@ -56,18 +58,27 @@ public class GetXandY {
 		
 		
 		//4th, save all data to a new file;
-		File output_file = new File("orc_output.txt");
+		File output_file = new File("orc_write_output_small.txt");
 		BufferedWriter output = new BufferedWriter(new FileWriter(output_file));
 		
+		output.write("Relative position, Total ChiP Signal" +"\n");
+		
 		for(int i=0; i<2001; i++){
+			
+			output.write(i-1000 + ",");
 			output.write(a[i] + "\n");
 		}
 		
-		//output.write("\n");
+	//	output.write("\n");
+		
 		output.close();
 		
 	}//end of main();
 
+	/*********
+	 * Printout an array, line by line;
+	 * @param a
+	 */
 	private static void printArray(int[] a) {
 		// TODO printout an array;
 		
