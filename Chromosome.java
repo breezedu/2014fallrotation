8e695dd8d4e0ef0f027bf1667093061d59451ff4
breezedu <breeze.du@gmail.com>
2014-09-15 15:59:54 -0400

    upload Chromosome.java class

---
+++ Chromosome.java
@@ -0,0 +1,14 @@
+package rotation2014fall;
+
+/*****
+ * Just a simple object to record the Chromosome name and relative position;
+ * @author Jeff
+ *
+ */
+
+public class Chromosome {
+	
+	String Chromo_name;
+	long Position;
+
+}
