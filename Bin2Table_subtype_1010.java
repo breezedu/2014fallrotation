cbbc484d47a8049a53f0e81ae334c50b395b943f
breezedu <breeze.du@gmail.com>
2014-10-10 13:34:48 -0400

    add Bin2Table_subtype_1010.java class

---
+++ Bin2Table_subtype_1010.java
@@ -0,0 +1,148 @@
+package rotation2014fall;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**********************
+ * Create a big table for each sub-type:
+ * Chr ,Start, Counts
+ * 2L, 1, xyz
+ * 2L, 1000, xyz
+ * .
+ * .
+ * .
+ * 2L, 23000000, xyz
+ * 2R, 1, xyz
+ * 2R, 1000, xyz,
+ * .
+ * .
+ * .
+ * 2R, 24000000, xyz
+ * 3L....
+ *
+ * @author Jeff
+ *
+ */
+
+public class Bin2Table_subtype_1010 {
+
+	public static void main(String[] args) throws IOException{
+		
+		System.out.println("This is Bin2Table script.");
+		
+		
+		//1st, read-in data from D:\2014FallRotation\data\yulong\1010;
+		String routine = "D:/2014FallRotation/data/yulong/1010/";		
+		
+		//readin the title of Bin document:
+		System.out.print("Please input the document to deal with: [DM456, DM457....DM463] \n document = ");
+		
+		Scanner input = new Scanner(System.in);
+		
+		String doc_name = input.next();
+		
+		
+		//array of chromosomes:
+		String[] chr_name = {"2L", "2R", "3L", "3R", "4", "X"};
+		
+		
+		//2nd, create an output_writer to write Bin document;
+		File output_file = new File(routine + "/Bin2Table/" + doc_name +"_Bin2Table_allChrs.txt");
+		BufferedWriter output = new BufferedWriter(new FileWriter(output_file));
+						
+		output.write("chr" + "\t" + "start_pos" + "\t" + "counts" + "\n");
+				
+		
+		//3rd, for each chromosome, write_in the big table;
+		for(int i=0; i<chr_name.length; i++){	
+			
+			run(routine, doc_name, chr_name[i], output);
+			
+		}//end for i<chr_name.length loop;
+		
+		
+		
+		//4th, close output writter;
+		output.close();
+		input.close();
+	}//end of main();
+
+	
+	
+	/*****************
+	 * 
+	 * @param routine
+	 * @param doc_name
+	 * @param chr_name
+	 * @param output
+	 * @throws IOException
+	 */
+	private static void run(String routine, String doc_name, String chr_name, BufferedWriter output) throws IOException {
+		// TODO Auto-generated method stub
+		
+		Scanner read_in = new Scanner(new File(routine +"/BED2Bin/" + doc_name + "_BED2Bin_" + chr_name +"_1000.txt"));
+		
+		String first_line = read_in.nextLine();
+		System.out.println("Line 106: FirstLine: " + first_line +"\n");
+		
+		//create a HashMap;
+		HashMap<Long, Integer> binHash = new HashMap<Long, Integer>();
+		long upper_bound = 0;
+		
+		while(read_in.hasNextLine()){
+			
+			String curr_line = read_in.nextLine();
+			
+			//split the line into chr_name, start, end;
+			String[] line_split = curr_line.split("\t");
+			
+			//when there are more than 3 items in that line, update the bin HashMap
+			if(line_split.length > 2){
+				
+				long bin_index = Integer.parseInt(line_split[1])/1000;
+				int count = Integer.parseInt(line_split[3]);
+				
+				binHash.put(bin_index, count);
+				
+				//update upper_bound;
+				if(bin_index > upper_bound) upper_bound = bin_index;
+				
+			}//end if line_split.length > 2 condition;
+			
+		}//end while loop;
+		
+		
+		
+		//after Building the HashMap, write_into Bin2Table.txt document;
+		long index = 0;
+		
+		while(index <= upper_bound){
+			
+			int count = 0;
+			//the window is within 10*current index; 
+			for(long j=index; j<index+10; j++){
+				
+				if(binHash.containsKey(j)){
+					count += binHash.get(j);
+				}
+			}
+			
+			output.write(chr_name +"\t" + index + "\t" + count + "\n");
+			
+			index++;
+		}//end while index <= upper_bound loop;
+		
+		
+		
+		//close read_in scanner;
+		read_in.close();
+		
+		
+	}//end run() method;
+	
+	
+}//end of everything in Bin2Table_subtype_1010 class;
