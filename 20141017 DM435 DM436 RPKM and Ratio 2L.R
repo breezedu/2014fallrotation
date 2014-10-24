edb9722117bed7ef195ab28cd52b541771f2c718
breezedu <breeze.du@gmail.com>
2014-10-24 11:11:00 -0400

    update DM435/DM436 RPKM and Ratio R-code

--- 20141017 DM435 DM436 RPKM and Ratio 2L.R
+++ 20141017 DM435 DM436 RPKM and Ratio 2L.R
@@ -1,10 +1,13 @@
 #Plot RPKM and ratio of DM436/DM435 Chromosome 2L over 10,000 bps region:
 
-par(mfrow = c(3, 1), mar = c(2, 4, 3, 2))
+par(mfrow = c(4, 1), mar = c(2, 4, 3, 2))
 
 
-#read table for DM436_2L_10000 data, 10000 means the window-width
-ratiotable36 = read.table("D:/2014FallRotation/data/yulong/1010/Bin2RPKM/DM436_Bin2RPKM_2L_10000R.txt", sep="\t", col.names=c("chr_id", "start", "reads"))
+###read table for https://alchemy.duhs.duke.edu/gd44~/Yulong/ ***
+###DM436_2L_10000 data, 10000 means the window-width
+###ratiotable36 = read.table("D:/2014FallRotation/data/yulong/1010/Bin2RPKM/DM436_Bin2RPKM_2L_10000R.txt", sep="\t", col.names=c("chr_id", "start", "reads"))
+
+ratiotable36 = read.table("http://alchemy.duhs.duke.edu/~gd44/Yulong/Yulong_1024/DM436_Bin2RPKM_2L_10000R.txt", sep="\t", col.names=c("chr_id", "start", "reads"))
 
 
 #plot the figure for DM436 chromosome 2L only (BLACK)
@@ -18,7 +21,7 @@ abline(h = 0, v = 0, col = "blue")
 
 ##############################################
 #read table for DM435_2L_10000 data
-ratiotable35 = read.table("D:/2014FallRotation/data/yulong/1010/Bin2RPKM/DM435_Bin2RPKM_2L_10000R.txt", sep="\t", col.names=c("chr_id", "start", "reads"))
+ratiotable35 = read.table("http://alchemy.duhs.duke.edu/~gd44/Yulong/Yulong_1024/DM435_Bin2RPKM_2L_10000R.txt", sep="\t", col.names=c("chr_id", "start", "reads"))
 
 
 #plot the figure for DM435 chromosome 2L only
@@ -32,9 +35,9 @@ abline(h = 0, v = 0, col = "blue")
 #plot DM435_2L and DM436_2L together, red for DM436, black for DM435
 
 #plot then points:
-plot(filter(ratiotable36$reads *1000, rep(1,15)/15), main="DM436(black) vs DM435(red), 10k RPKM ",cex=.3)
+plot(ratiotable36$reads *1000, main="DM436(black) vs DM435(red), 10k RPKM ",cex=.3)
 
-points(filter(ratiotable35$reads*1000, rep(1,15)/15), main="DM436(black) vs DM435(red), 10k",cex=.3, col="red")
+points(ratiotable35$reads*1000, main="DM436(black) vs DM435(red), 10k",cex=.3, col="red")
 
 
 #create a new window Without filter;
