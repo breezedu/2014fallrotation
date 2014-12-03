76409807d67718645042a8a245db7d5d6f4d9bab
breezedu <breeze.du@gmail.com>
2014-12-03 14:00:20 -0500

    update 20141203_OverlapH2AvandTime.R code

--- 20141203_OverlapH2AvandTime.R
+++ 20141203_OverlapH2AvandTime.R
@@ -15,22 +15,22 @@ ratiotable005 = read.table("http://alchemy.duhs.duke.edu/~gd44/Yulong/Yulong_111
 
 ########################################################
 # plot all three figures in one figure.
-# plot part one:
+# plot part one, cutoff = 0.5
 ########################################################
 X11()
-tiff(file="h2avandtiming.tif")       ## make a new pnd document;
+png(file="H2AvTimingOverlap.png")       ## make a new png figure;
 
 par(mfrow = c(3, 1), mar = c(2, 4, 3, 2))
 
-#plot the figure for cutoff=0.5 
+#plot the H2Av figure for cutoff=0.5
 title1 = ( "cutoff 1.0")
 plot((ratiotable10$reads),type = "l", main= title1, cex=.3, xlim = c(0, 100))
+
 par(new=T) 
+#plot the Timing figure for cutoff=0.5
+ratiotableTime0.5 = read.table("http://alchemy.duhs.duke.edu/~gd44/Yulong/Yulong_1203/StretchRegionWithTime_cutoff0.0.txt", sep="\t", col.names=c("index", "reads"))
 
-#plot the figure for cutoff=1.0 
-ratiotableTime0.5 = read.table("http://alchemy.duhs.duke.edu/~gd44/Yulong/Yulong_1203/StretchRegionWithTime_cutoff0.5.txt", sep="\t", col.names=c("index", "reads"))
-
-title1 = ( "cutoff 0.0001")
+title1 = ( "cutoff 0.5")
 plot((ratiotableTime0.5$reads),type = "l",ann=F, cex=.3, xaxt="n", yaxt="n", col = "blue", xlim=c(0, 100))
 legend("topright", legend = c("timing", "H2Av"), lty = 1, lwd=2, col=c("blue", "black") )
 
@@ -59,7 +59,7 @@ legend("topright", legend = c("timing", "H2Av"), lty = 1, lwd=2, col=c("blue", "
 
 ########################################################
 # the third part
-#plot the figure for cutoff=0.05
+# plot the figure for cutoff=0.05
 ########################################################
 
 title3 = ( "cutoff 0.05")
@@ -77,24 +77,24 @@ legend("topright", legend = c("timing", "H2Av"), lty = 1, lwd=2, col=c("blue", "
 
 
 #close the png figure plotting
-dev.off()
+#dev.off()
 
 
 #############################
 #plot seperate figures:
 
-X11()
+#X11()
 #plot the figure for cutoff=1.0 
-title1 = ( "cutoff 1.0")
-plot((ratiotable10$reads),type = "l", main= title1, cex=.3)
+#title1 = ( "cutoff 1.0")
+#plot((ratiotable10$reads),type = "l", main= title1, cex=.3)
 
-X11()
+#X11()
 #plot the figure for cutoff=0.5 
-title2 = ( "cutoff 0.5")
-plot((ratiotable05$reads),type = "l", main= title2, cex=.3)
+#title2 = ( "cutoff 0.5")
+#plot((ratiotable05$reads),type = "l", main= title2, cex=.3)
 
-X11()
+#X11()
 #plot the figure for cutoff=0.05
-title3 = ( "cutoff 0.05")
-plot((ratiotable005$reads),type = "l", main= title3, cex=.3)
+#title3 = ( "cutoff 0.05")
+#plot((ratiotable005$reads),type = "l", main= title3, cex=.3)
 
