95f2e303c4be8cce4403a75a3024f604714d4327
breezedu <breeze.du@gmail.com>
2014-12-03 09:46:40 -0500

    pushed 20141203_overlapwithTimeandStretchAllRegions.R code

--- 20141203_OverlapwithTimeAndStretchAllRegions.R
+++ 20141203_OverlapwithTimeAndStretchAllRegions.R
@@ -8,7 +8,7 @@
 #Read data:
 ratiotable10 = read.table("http://alchemy.duhs.duke.edu/~gd44/Yulong/Yulong_1203/StretchRegionWithTime_cutoff1.0E-4.txt", sep="\t", col.names=c("index", "reads"))
 
-ratiotable05 = read.table("http://alchemy.duhs.duke.edu/~gd44/Yulong/Yulong_1203/StretchRegionWithTime_cutoff0.5.txt", sep="\t", col.names=c("index", "reads"))
+ratiotable05 = read.table("http://alchemy.duhs.duke.edu/~gd44/Yulong/Yulong_1203/StretchRegionWithTime_cutoff0.05.txt", sep="\t", col.names=c("index", "reads"))
 
 ratiotable005 = read.table("http://alchemy.duhs.duke.edu/~gd44/Yulong/Yulong_1203/StretchRegionWithTime_cutoff0.0.txt", sep="\t", col.names=c("index", "reads"))
 
@@ -19,16 +19,16 @@ X11()
 par(mfrow = c(3, 1), mar = c(2, 4, 3, 2))
 
 #plot the figure for cutoff=1.0 
-title1 = ( "cutoff 1.0")
+title1 = ( "cutoff 0.0001")
 plot((ratiotable10$reads),type = "l", main= title1, cex=.3)
 
 #plot the figure for cutoff=0.5 
-title2 = ( "cutoff 0.5")
+title2 = ( "cutoff 0.05")
 plot((ratiotable05$reads),type = "l", main= title2, cex=.3)
 
 
 #plot the figure for cutoff=0.05
-title3 = ( "cutoff 0.05")
+title3 = ( "cutoff 0")
 plot((ratiotable005$reads),type = "l", main= title3, cex=.3)
 
 
@@ -37,16 +37,16 @@ plot((ratiotable005$reads),type = "l", main= title3, cex=.3)
 
 X11()
 #plot the figure for cutoff=1.0 
-title1 = ( "cutoff 1.0")
+title1 = ( "cutoff 0.0001")
 plot((ratiotable10$reads),type = "l", main= title1, cex=.3)
 
 X11()
 #plot the figure for cutoff=0.5 
-title2 = ( "cutoff 0.5")
+title2 = ( "cutoff 0.05")
 plot((ratiotable05$reads),type = "l", main= title2, cex=.3)
 
 X11()
 #plot the figure for cutoff=0.05
-title3 = ( "cutoff 0.05")
+title3 = ( "cutoff 0")
 plot((ratiotable005$reads),type = "l", main= title3, cex=.3)
 
